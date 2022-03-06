c1d7a0c70844019d11451b503467bc206a0bb706
Ferid HELALI <helaliferid@gmail.com>
2022-03-06 10:46:28 +0100

    add error midle ware

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const res = require('express/lib/response');
+
+const app=express()
+
+
+
+app.get('/',[mlError,m1,m2],function(req,res){
+    console.log(req.passedValue);
+
+    console.log('GET / request Handler is running');
+    res.json({message:"API response on GET /"})
+})
+
+
+
+app.listen(3000,function(){
+    console.log('Server Listening on http://localhost:3000')
+})
+
+function m1(req,res,next){
+    let passedValue={passport:'Ok'};
+    req.passport=passedValue;
+    throw Error('Hi i am an error')
+    console.log('running th m1 middleware');
+    next()
+}
+
+function m2(req,res,next){
+    req.passedValue={message:":("}
+    console.log('running th m2 middleware');
+    next()
+}
+
+function mlError(error,req,res,next){
+    console.log('Error middleware in action');
+    res.json({error:'Error from the mlError midleware',error})
+}
